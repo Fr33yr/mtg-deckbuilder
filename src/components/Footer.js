12020f03d0fe68a3ac40bc08de2bd90795595887
Agustin Cristobo <fr33yr@gmail.com>
2022-07-17 14:43:02 -0300

    link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
-import React,{Fragment} from 'react'
+import React, { Fragment } from 'react'
 
 import '../assets/css/main.css'
 
 export default function Footer() {
   return (
     <Fragment>
-        <footer>
-          <h2>Footer</h2>
-        </footer>
+      <footer>
+        <div>
+          <p>Powered by</p>
+          <a href='https://magicthegathering.io' target={'_blank'}>The MTG API</a>
+        </div>
+      </footer>
     </Fragment>
   )
 }
