2b473d575f40853991f990ebd6918490ec5196e3
Agustin Cristobo <fr33yr@gmail.com>
2022-07-20 09:44:36 -0300

    ...

---
+++ src/utils/reducerFunctions.js
@@ -0,0 +1,32 @@
+export function hasItem(arry, id) {
+    return arry.find(item => item.id === `${id}`)
+}
+
+export function removeItem(arr, id){
+    const index = arr.findIndex(object => {
+        return object.id === id;
+    })
+    arr.splice(index,1)
+}
+
+export function addAmount(arr, id) {
+    const index = arr.findIndex(object => {
+        return object.id === id;
+    });
+    if (index !== -1) {
+        arr[index].amount += 1;
+    }
+}
+
+export function decreaseAmount(arr, id) {
+    const index = arr.findIndex(object => {
+        return object.id === id;
+    });
+    if (index !== -1) {
+        arr[index].amount -= 1;
+    }
+}
+
+export function setName(name){
+    return name
+}
