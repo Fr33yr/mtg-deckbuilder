40dabf2f3b8d849771d918a16200be4b058f796f
Agustin Cristobo <fr33yr@gmail.com>
2022-07-29 10:43:26 -0300

    lands bug fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,14 +6,21 @@ import { addCards } from '../store/deckSlice'
 
 
 export function Card(props) {
-  const { name, imageUrl, id, colors } = props
+  let { name, imageUrl, id, colors } = props
   const dispatch = useDispatch()
   let amount = 1
+
+  const handleOnClick = () =>{
+    if(!colors){
+      colors = ""
+    }
+    dispatch(addCards({ name, amount, imageUrl, colors, id }))
+  }
 
   return (
     <Fragment>
       <div className="card">
-        <button className='card-btn' onClick={() => dispatch(addCards({ name, amount, imageUrl, colors, id }))}>+</button>
+        <button className='card-btn' onClick={handleOnClick}>+</button>
         <img src={imageUrl} alt="mtg-card" unselectable='on'
           className='card-img' />
       </div>
