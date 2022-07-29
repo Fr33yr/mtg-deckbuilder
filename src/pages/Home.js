bd6406ea7a4bef6ccff4607cc52f9c5b2b6e4673
Agustin Cristobo <fr33yr@gmail.com>
2022-07-29 18:17:54 -0300

    decks counter and more styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,47 @@
 import React, { Fragment, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import {useDispatch} from 'react-redux'
-import {Toaster, toast} from 'react-hot-toast'
+import { useDispatch } from 'react-redux'
+import { Toaster, toast } from 'react-hot-toast'
 
 import { useAuth } from '../context/AuthContext'
 import { useFetch } from '../hooks/useFetch'
 import { DeckItem } from '../components/DeckItem'
-import {reset} from '../store/deckSlice'
+import { reset } from '../store/deckSlice'
 
 
 export function Home(props) {
   const { user } = useAuth()
   const [data, setData] = useFetch(user)
-  const {setSearchResults} = props
+  const { setSearchResults } = props
   const dispatch = useDispatch()
   let location = useLocation()
-  
-  useEffect(()=>{
-    if(location.pathname === '/'){
+
+  useEffect(() => {
+    if (location.pathname === '/') {
       setSearchResults([])
     }
-  },[location])
+  }, [location])
 
-  const handleOnClick = () =>{
+  const handleOnClick = () => {
     dispatch(reset())
   }
 
   return (
     <Fragment>
       <Toaster
-      position='top-center'/>
+        position='top-center' />
+
       <div className='home'>
-        <Link to="/deckbuilder" className='newdeck'
-        onClick={handleOnClick}>New <br />Deck</Link>
+        <div className="newdeck">
+          <p>{data && data.length}/8</p>
+          <Link to="/deckbuilder" onClick={handleOnClick} style={{}}>
+            New <br />Deck
+          </Link>
+        </div>
         {
-          data && data.map((deck) => 
-          <DeckItem {...deck} user={user} key={deck.id}
-          data={data} setData={setData}/>)
+          data && data.map((deck) =>
+            <DeckItem {...deck} user={user} key={deck.id}
+              data={data} setData={setData} />)
         }
       </div>
     </Fragment>
